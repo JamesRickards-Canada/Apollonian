a59d2b5221f40d38b9b84acf6aa65ab8fb81b141
JamesRickards-Canada <james.rickards@colorado.edu>
2023-06-29 11:31:08 -0600

    Corrected apol_missing to better account for symmetries, those cases are now faster.

--- apol_fast.c
+++ apol_fast.c
@@ -26,7 +26,7 @@ static int iscop(long n, long cop);
 
 /*1: C CODE*/
 
-/*Finds all missing positive curvatures in the given residue classes, saving them to a file..*/
+/*Finds all missing positive curvatures in the given residue classes, saving them to a file. Formatting of the inputs is provided by apol_missing; it is crucial that x is reduced, sorted, and res is the set of ALL residues modulo 24.*/
 static void
 findmissing(long B, long x[], long res[], long lenres, int families)
 {
@@ -69,30 +69,38 @@ findmissing(long B, long x[], long res[], long lenres, int families)
 	long u = a / 64;/*a=64u+v. u gives the entry of the array, v gives the bit to swap.*/
 	rclass[b][u] |= bitswap[v];
   }
-  /*We adjust the starting quadruple in case of symmetries.*/
-  if (x[0] == 0) {/*We are the [0, 0, 1, 1] packing: do [1, 4, 0, 1] and start with swapping the third.*/
-	depthseq[0][0] = 1; depthseq[0][1] = 4; depthseq[0][2] = 0; depthseq[0][3] = 1;
+  /*We adjust the starting quadruple in case of symmetries: for a+b+c=d, we put d first, and DO NOT flip it on the first iteration. If c=d, we do c, a, c, b, starting with the second one. Until one element of the depth sequence flips the third entry, we do not flip the first one, as they will be (c, c) still. There are two exceptions: [0, 0, 1, 1], and [-1, 2, 2, 3], as they have both types of symmetry. For the secon, we do 2, 3, 2, -1, and start at the third entry. For [0, 0, 1, 1], we do [1, 4, 1, 0] and also start at the third one (we do the first move, since it is forced). The variable sym keeps track of this: -1 means no symmetries, don't worry. 0 means symmetris and we have not moved beyond them, hence we cannot flip the first element. >0 means this is the index of depthseq that the first 3 occurs, so we know when we drop back into the symmetry zone.
+  */
+  long sym;
+  if (x[0] == 0) {/*We are the [0, 0, 1, 1] packing: do [1, 4, 1, 0] and start with swapping the third.*/
+	depthseq[0][0] = 1; depthseq[0][1] = 4; depthseq[0][2] = 1; depthseq[0][3] = 0;
 	rclass[4][0] |= bitswap[0];/*Since we are adjusting, we did not do 4 yet.*/
 	swaps[1] = 1;/*Will get incremented to 2 right away.*/
+	sym = 0;/*Symmetries to avoid.*/
   }
-  else if (x[0] == -1) {/*[-1, 2, 2, 3]: do [2, 3, -1, 2]*/
-    depthseq[0][0] = 2; depthseq[0][1] = 3; depthseq[0][2] = -1; depthseq[0][3] = 2;
-	swaps[1] = 1;  
+  else if (x[0] == -1) {/*[-1, 2, 2, 3]: do [2, 3, 2, -1]*/
+    depthseq[0][0] = 2; depthseq[0][1] = 3; depthseq[0][2] = 2; depthseq[0][3] = -1;
+	swaps[1] = 1;
+	sym = 0;/*Symmetries to avoid.*/
   }
   else if (x[1] == x[2]) {/*b=c, so do [b, a, b, d]*/
 	depthseq[0][0] = x[1]; depthseq[0][1] = x[0]; depthseq[0][2] = x[2]; depthseq[0][3] = x[3];
 	swaps[1] = 0;
+	sym = 0;/*Symmetries to avoid.*/
   }
-  else if (x[2] == x[3]) {/*c=d, so do [c, a, b, c]*/
-	depthseq[0][0] = x[3]; depthseq[0][1] = x[0]; depthseq[0][2] = x[1]; depthseq[0][3] = x[2];
+  else if (x[2] == x[3]) {/*c=d, so do [c, a, c, b]*/
+	depthseq[0][0] = x[3]; depthseq[0][1] = x[0]; depthseq[0][2] = x[2]; depthseq[0][3] = x[1];
 	swaps[1] = 0;
+	sym = 0;/*Symmetries to avoid.*/
   }
   else if ((x[0] + x[1] + x[2]) == x[3]) {/*a+b+c=d, so do [d, a, b, c]*/
 	depthseq[0][0] = x[3]; depthseq[0][1] = x[0]; depthseq[0][2] = x[1]; depthseq[0][3] = x[2];
 	swaps[1] = 0;
+	sym = -1;/*After first swap, no symmetries to worry about.*/
   }
   else {
 	depthseq[0][0] = x[0]; depthseq[0][1] = x[1]; depthseq[0][2] = x[2]; depthseq[0][3] = x[3];
+	sym = -1;/*No symmetries to begin with.*/
   }
   long ind = 1;/*Which depth we are working at.*/
   while (ind > 0) {/*We are coming in trying to swap this circle out.*/
@@ -100,15 +108,23 @@ findmissing(long B, long x[], long res[], long lenres, int families)
 	if (cind == 4) {/*Overflowed, go back.*/
 	  swaps[ind] = -1;
 	  ind--;
+	  if (ind < sym) sym = 0;/*We moved past the first index swapping 3, so worry about symmetries again.*/
 	  continue;
 	}
 	long lastind = ind - 1;
 	if (cind == swaps[lastind]) continue; /*Same thing twice, so skip it.*/
+	if (!sym) {/*Worry about symmetries.*/
+	  if (cind == 0) continue;/*We skip the first one.*/
+	  else if (cind == 2) sym = ind + 1;/*First time we swap out the third one, eliminating symmetries further on in this branch.*/
+	}
 	long apbpc = 0;/*Now we can reasonably try a swap.*/
 	for (i = 0; i < cind; i++) apbpc += depthseq[lastind][i];
 	for (i = cind + 1; i < 4; i++) apbpc += depthseq[lastind][i];
 	long newc = (apbpc << 1) - depthseq[lastind][cind];/*2(a+b+c)-d, the new curvature.*/
-	if (newc > B) continue;/*Too big! go back.*/
+	if (newc > B) {/*Too big! go back.*/
+	  if (ind < sym) sym = 0;/*Tried flipping out of symmetry here but it's too big.*/
+	  continue;
+	}
 	/*Do the bitswap to update the count.*/
 	long b = newc % 24;
 	long a = newc / 24;/*newc=24a+b. b gives the block to insert into, and we need to save "a"*/
